cc69ed0922ca26b9abe0462e6bddcf89ecf5c361
Igor Santos Mascarenhas <46729174+igorsmasc@users.noreply.github.com>
2023-11-13 22:23:42 -0300

    Add tests

--- src/main/java/com/ada/banco/domain/usecase/CriarNovaConta.java
+++ src/main/java/com/ada/banco/domain/usecase/CriarNovaConta.java
@@ -11,16 +11,10 @@ public class CriarNovaConta {
     }
 
     public Conta execute(Conta conta) throws Exception {
-        // validar se o usuario ja possui uma conta
         if(contaGateway.buscarPorCpf(conta.getCpf()) != null) {
-            // - se possuir vamos lancar uma exception
             throw new Exception("Usuario ja possui uma conta");
         }
-
-        // criar uma nova conta
         Conta novaConta = contaGateway.salvar(conta);
-
-        //return Conta;
         return novaConta;
     }
 }
---
+++ src/test/java/com/ada/banco/dummy/ContaGatewayDummyImpl.java
@@ -0,0 +1,22 @@
+package com.ada.banco.dummy;
+
+import com.ada.banco.domain.gateway.ContaGateway;
+import com.ada.banco.domain.model.Conta;
+
+import java.math.BigDecimal;
+
+public class ContaGatewayDummyImpl implements ContaGateway {
+    @Override
+    public Conta buscarPorCpf(String cpf) {
+        if(cpf.equals("123456789")) {
+            return new Conta(1L, 2L, 3L, BigDecimal.ZERO, "Pedro", "123456789");
+        }
+
+        return null;
+    }
+
+    @Override
+    public Conta salvar(Conta conta) {
+        return new Conta(1L, 2L, 3L, BigDecimal.ZERO, "Pedro", "123456789");
+    }
+}
